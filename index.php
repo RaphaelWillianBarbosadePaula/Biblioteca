5c618348693e2b5ae819a8ba672b7dd5355ace92
Raphael Willian Barbosa de Paula <120695571+RaphaelWillianBarbosadePaula@users.noreply.github.com>
2023-03-09 14:02:42 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,71 @@
+<?php
+
+include("views/header.php");
+include("views/menu.php");
+include("views/footer.php");
+
+?>
+
+
+
+<div class="container">
+<br>
+<nav class="navbar navbar-light" style="background-color: #e3f2fd;">
+  <div class="container-fluid">
+    <span class="navbar-brand mb-0 h1">A seguir, veja algumas recomendações para você!</span>
+  </div>
+</nav>
+<hr>
+
+<div class="row">
+
+<div class="col-md-3">
+<div class="card" style="width: 18rem;">
+  <img src="https://images-na.ssl-images-amazon.com/images/I/81ibfYk4qmL.jpg" class="card-img-top">
+  <div class="card-body">
+    <h5 class="card-title">Harry Potter e a Pedra Filosofal</h5>
+    <p class="card-text">O livro conta a história de Harry Potter, um órfão criado pelos tios que descobre, em seu décimo primeiro aniversário, que é um bruxo.</p>
+    <a href="https://www.amazon.com.br/Harry-Potter-Pedra-Filosofal-Rowling/dp/8532530788" class="btn btn-primary">Adquira já!</a>
+  </div>
+</div>
+
+</div>
+
+<div class="col-md-3">
+<div class="card" style="width: 18rem;">
+  <img src="https://images-na.ssl-images-amazon.com/images/I/41CPMP5rmZL._SX342_SY445_QL70_ML2_.jpg" class="card-img-top">
+  <div class="card-body">
+    <h5 class="card-title">O poder da liderança positiva</h5>
+    <p class="card-text">Ser um líder positivo não é apenas uma boa maneira de liderar. É o jeito certo de liderar se você deseja construir uma excelente cultura...</p>
+    <a href="https://www.amazon.com.br/poder-lideran%C3%A7a-positiva-Jon-Gordon/dp/8550802859" class="btn btn-primary">Adquira já!</a>
+  </div>
+</div>
+
+</div>
+
+<div class="col-md-3">
+<div class="card" style="width: 18rem;">
+  <img src="https://images-na.ssl-images-amazon.com/images/I/61OcS3TpIkL._SX350_BO1,204,203,200_.jpg" class="card-img-top">
+  <div class="card-body">
+  <b><h5 class="card-title">Sagarana</h5></b>
+    <p class="card-text">Cenários e personagens típicos do interior do país, mais especificamente do sertão de Minas Gerais. A linguagem inventiva do livro é outro aspecto que distinguiria para sempre o autor no campo da literatura brasileira. </p>
+    <a href="https://www.amazon.com.br/Sagarana-Jo%C3%A3o-Guimar%C3%A3es-Rosa/dp/8526024647/ref=asc_df_8526024647/?tag=googleshopp00-20&linkCode=df0&hvadid=379739056264&hvpos=&hvnetw=g&hvrand=3075752009778540872&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9074182&hvtargid=pla-809695662620&psc=1" class="btn btn-primary">Adquira já!</a>
+  </div>
+</div>
+
+</div>
+
+<div class="col-md-3">
+<div class="card" style="width: 18rem;">
+  <img src="https://images-na.ssl-images-amazon.com/images/I/51mJcEToALL._SX343_BO1,204,203,200_.jpg" class="card-img-top">
+  <div class="card-body">
+    <h5 class="card-title">One-Punch Man - Catálogo de Heróis</h5>
+    <p class="card-text">Enciclopédia que contém informações sobre os heróis da Associação de Heróis, com seus rankings atualizados até o arco da Caça aos Heróis.</p>
+    <a href="https://www.amazon.com.br/One-punch-Man-Cat%C3%A1logo-Her%C3%B3is-One/dp/8542626931/ref=asc_df_8542626931/?tag=googleshopp00-20&linkCode=df0&hvadid=418751331321&hvpos=&hvnetw=g&hvrand=9328804838864468517&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9074182&hvtargid=pla-882904775902&psc=1" class="btn btn-primary">Adquira já!</a>
+  </div>
+</div>
+
+</div>
+
+</div>
+</div>
